5130ab26a95d87e81940def2f26eb683b5b44e42
Luboš Pokorný <zoido@users.noreply.github.com>
2026-08-01 22:44:38 +0200

    cpy to clipboard

--- internal/dispatch/dispatch.go
+++ internal/dispatch/dispatch.go
@@ -1,6 +1,9 @@
 package dispatch
 
 import (
+	"os"
+
+	osc52 "github.com/aymanbagabas/go-osc52/v2"
 	"github.com/rkoesters/xdg"
 
 	"github.com/zoido/gou/internal/domain/finding"
@@ -18,6 +21,9 @@ func (d *Dispatcher) Dispatch(key string, f finding.Finding) (bool, error) {
 	switch key {
 	case "enter":
 		return false, xdg.Open(f.URL())
+	case "y":
+		_, err := osc52.New(f.URL()).WriteTo(os.Stderr)
+		return false, err
 	}
 
 	return doQuit, nil
