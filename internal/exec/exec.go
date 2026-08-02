34de29343e3011b1bbd9108fac9c9b0f62960812
Luboš Pokorný <zoido@users.noreply.github.com>
2026-08-02 17:22:46 +0200

    feat: does nothing (#11)
    Initial feature PR to trigger the release pipeline and have something to
    build upon.

--- internal/exec/exec.go
+++ internal/exec/exec.go
@@ -2,11 +2,15 @@ package exec
 
 import (
 	"context"
+
+	"github.com/zoido/gou/internal/exec/app"
 )
 
 // Exec is the entry point for the gou command.
 func Exec(ctx context.Context, args []string) int {
-	_ = ctx
-	_ = args
+	err := app.Run(ctx, args)
+	if err != nil {
+		return 1
+	}
 	return 0
 }
