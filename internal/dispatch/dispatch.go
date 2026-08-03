c2752a857ec15d2e27ce2cb4ab44df0b0873586e
Luboš Pokorný <zoido@users.noreply.github.com>
2026-08-03 19:52:25 +0200

    refactor: implement dispatching of the actions
    added dispatcher is supposed to perform actions on keypresses

---
+++ internal/dispatch/dispatch.go
@@ -0,0 +1,20 @@
+package dispatch
+
+import (
+	"github.com/zoido/gou/internal/model"
+)
+
+// Dispatcher decides what action to perform based on the key press.
+type Dispatcher map[string]Action
+
+// Action represents single action done with th single [model.Finding]. Returned true indicates that
+// application should end when done.
+type Action func(model.Finding) (bool, error)
+
+// Dispatch performs the action associated with the key press and returns its result.
+func (d Dispatcher) Dispatch(key string, f model.Finding) (bool, error) {
+	if a, ok := d[key]; ok {
+		return a(f)
+	}
+	return false, nil
+}
