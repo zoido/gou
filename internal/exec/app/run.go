9afed464dd365982421282b706d6d65b2d9ca4ee
Luboš Pokorný <zoido@users.noreply.github.com>
2026-08-04 21:34:46 +0200

    feat: print out found URLs
    prepare the skaffolding for the app and log out found URLs

--- internal/exec/app/run.go
+++ internal/exec/app/run.go
@@ -2,11 +2,111 @@ package app
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/zoido/gou/internal/exec/app/config"
+	"github.com/zoido/gou/internal/exec/app/factory"
+	"github.com/zoido/gou/internal/scan"
+)
+
+var (
+	shutdownTimeout   = 10 * time.Second
+	hardShutdownDelay = 3 * time.Second
 )
 
 // Run sets up and start the GoU application.
 func Run(ctx context.Context, args []string) error {
-	_ = ctx
-	_ = args
+	ctx, stopHandlingSignals := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stopHandlingSignals()
+
+	cfg, err := config.ParseConfig(args)
+	if usageErr, shouldPrintUsage := errors.AsType[config.PrintUsageError](err); shouldPrintUsage {
+		fmt.Println(usageErr.Usage)
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("failed to parse config: %v", err)
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			slog.Error("GoU panic", "panic", r)
+		}
+	}()
+
+	f := factory.New(cfg)
+	slog.SetDefault(slog.New(f.SlogHandler()))
+
+	slog.Debug("GoU config", "config", cfg)
+
+	open := f.InputOpener()
+	reader, err := open()
+	if err != nil {
+		return fmt.Errorf("opening input: %v", err)
+	}
+
+	closeInput := sync.OnceFunc(func() {
+		if err := reader.Close(); err != nil {
+			slog.Warn("Failed closing input", "error", err)
+		}
+	})
+
+	grp, grpCtx := errgroup.WithContext(ctx)
+	success := make(chan struct{})
+	grp.Go(func() error {
+		findings, err := scan.FindURLs(grpCtx, reader)
+		closeInput() // We don't need it anymore so we do not need to hold it.
+		if err != nil {
+			return err
+		}
+
+		for _, f := range findings {
+			slog.Info("Found URL", "url", f.URL())
+		}
+		close(success)
+		return nil
+	})
+
+	select {
+	case <-grpCtx.Done():
+		slog.Error("Error in execution, shutting down.")
+	case <-ctx.Done():
+		slog.Debug(fmt.Sprintf("%v, shutting down", context.Cause(ctx)))
+	case <-success:
+		// All good just shutdown.
+	}
+
+	stopHandlingSignals()
+
+	go func() {
+		closeInput()
+	}()
+
+	slog.Debug("Waiting for app to shutdown.")
+	errCh := make(chan error)
+	go func() {
+		errCh <- grp.Wait()
+	}()
+
+	select {
+	case err := <-errCh:
+		if err != nil {
+			return fmt.Errorf("error: %v", err)
+		}
+	case <-time.After(shutdownTimeout):
+		slog.Debug("Shutdown timed out, stopping forcefully.")
+		slog.Debug("Waiting before exit", "delay", hardShutdownDelay)
+		time.Sleep(hardShutdownDelay)
+	}
+
 	return nil
 }
